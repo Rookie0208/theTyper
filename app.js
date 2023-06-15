2524e9927ab3fba2155163a5bf15fed0a2491739
Amit Rawat <68739013+Rookie0208@users.noreply.github.com>
2023-06-15 18:42:05 +0530

    updated typing content

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
-const opt = document.getElementById("15");
-    const time = document.getElementById("time");
-    const word = document.getElementById("word");
-    const colorBtn = document.querySelectorAll("h3");
+// const opt = document.getElementById("15");
+//     const time = document.getElementById("time");
+//     const word = document.getElementById("word");
+//     const colorBtn = document.querySelectorAll("h3");
 
-    colorBtn.forEach((ele) => {
-        ele.addEventListener("click", (t) => {
-            ele.classList.add('active');
-            // ele2.classList.remove('active');
-        // opt.classList.remove('active');
-    })
-    })
+//     colorBtn.forEach((ele) => {
+//         ele.addEventListener("click", (t) => {
+//             ele.classList.add('active');
+//             // ele2.classList.remove('active');
+//         // opt.classList.remove('active');
+//     })
+//     })
+
+
+const newText = document.querySelector('.typing-text p');
+const inpField = document.querySelector('.input-field');
+
+let charInd = 0;
+window.onload = () => {
+    text.innerHTML = "";
+    let randInd = Math.floor(Math.random() * paragraphs.length);
+    (paragraphs[randInd].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        text.innerHTML += (spanTag);
+    }));
+
+    document.addEventListener("keydown", () => inpField.focus());
+    text.addEventListener("keydown", () => inpField.focus());
+}
+nextText.addEventListener('click', () => {
+    text.innerHTML = "";
+    let randInd = Math.floor(Math.random() * paragraphs.length);
+    (paragraphs[randInd].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        text.innerHTML += (spanTag);
+    }));
+
+    document.addEventListener("keydown", () => inpField.focus());
+    text.addEventListener("keydown", () => inpField.focus());
+})
+
+function initTyping() {
+    const characters = text.querySelectorAll('span');
+    let typedChar = inpField.value.split("")[charInd];
+    if (characters[charInd].innerText == typedChar) {
+        characters[charInd].classList.add("correct");
+    } else {
+        characters[charInd].classList.add("incorrect");
+    }
+    charInd++;
+    characters.forEach(span => span.classList.remove("activeBar"));
+    characters[charInd].classList.add("activeBar");
+    // console.log(charInd);
+
+}
+
+inpField.addEventListener('input', initTyping);
--- content.js
+++ content.js
@@ -39,6 +39,8 @@ nextText.onclick = () => {
     // if(check == 0) {
     //     text.innerHTML = paragraphs[Math.floor(Math.random() * paragraphs.length)];
     // }
+    charInd = 0;
+    inpField.value = "";
     if(check == 15 || check == 0) {
         text.innerHTML = paragraphs[Math.floor(Math.random() * paragraphs.length)].split(' ').slice(0,15).join(' ');
     }
@@ -48,14 +50,33 @@ nextText.onclick = () => {
 }
 
 word15.addEventListener('click', () => {
-    if(check == 0 || check == 25) {
-        text.innerHTML = paragraphs[Math.floor(Math.random() * paragraphs.length)].split(' ').slice(0,15).join(' ');
+    charInd = 0;
+    let count = 0;
+    if (check == 25 || check == 0) {
+        text.innerHTML = "";
+        let randInd = Math.floor(Math.random() * paragraphs.length);
+        let newParagraph = paragraphs[randInd];
+        paragraphs[randInd].split("").forEach(span => {
+            if(count <= 15) {
+                let spanTag = `<span>${span}</span>`;
+                text.innerHTML += (spanTag);
+                if(span === ' ') {
+                    count++;
+                }
+            }
+        });
+        // text.innerHTML = newParagraph.split(" ").slice(0,15).join(' ');
+        // console.log(newParagraph);
     }
     check = 15;
 })
 word25.addEventListener('click', () => {
+    charInd = 0;
+    inpField.value = "";
     if(check == 0 || check == 15) {
         text.innerHTML = paragraphs[Math.floor(Math.random() * paragraphs.length)].split(' ').slice(0,25).join(' ');
     }
     check = 25;
 })
+
+// loadPara();
